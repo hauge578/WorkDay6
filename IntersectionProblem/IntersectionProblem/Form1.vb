de28ee968f9d87410be8f5b7e742b4c5d5a0ca73
idsc3101 <idsc3101@gmail.com>
2014-10-06 17:08:25 -0500

    Finished Wkdy

--- IntersectionProblem/IntersectionProblem/Form1.vb
+++ IntersectionProblem/IntersectionProblem/Form1.vb
@@ -26,8 +26,21 @@ Public Class Form1
         D = CDbl(Dst)
 
         ' Please write your code here...
+        Dim result As String
 
-        txtResult.Text = "Not implemented yet"
+        If (C > B) Or (A > D) Then
+            result = "They don't intersect"
+        Else
+            Dim begPoint As Double = Math.Max(A, C)
+            Dim endPoint As Double = Math.Min(B, D)
+
+
+            result = "They do intersect from " & begPoint & " to " & endPoint
+        End If
+        txtResult.Text = result
     End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -3,9 +3,29 @@ Public Class Form1
 
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
+        Dim aSt As String = txtSideOne.Text
+        Dim bSt As String = txtSideTwo.Text
 
-        txtHyp.Text = "Not Implemented Yet"
+        Dim a As Double = CDbl(aSt)
+        Dim b As Double = CDbl(bSt)
+
+        Dim res As Double
+        res = Hypotenuse(a, b)
+
+
+
+        txtHyp.Text = CStr(res)
 
     End Sub
+
+    Function Hypotenuse(ByVal x As Double,
+                        ByVal y As Double) As Double
+
+
+        Dim result As Double
+        result = Math.Sqrt(x ^ 2 + y ^ 2)
+        Return result
+
+    End Function
 
 End Class
